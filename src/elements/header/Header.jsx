3e5e922fe15461d42b43d1e9560ddee044f71ffb
ITea0fc <ilyatarasov@bk.ru>
2024-01-17 18:35:33 +0700

    Clean header code

--- src/elements/header/Header.jsx
+++ src/elements/header/Header.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import "./header.scss";
-import Logo, { AirPlane } from '../../assets';
+import {AirPlane} from '../../assets';
 
 export default class Header extends React.Component {
     render() {
